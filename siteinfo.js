f3e196c30bd2e33d572f5c312e92634b6bb17cc8
lgt <563547352@qq.com>
2019-06-05 17:10:38 +0800

    222

---
+++ siteinfo.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: "小程序商城",
+  uniacid: "5",
+  acid: "5",
+  multiid: "0",
+  version: "2.9.38",
+  siteroot: "https://www.ipatr.cn/app/index.php",
+  apiroot: "https://www.ipatr.cn/web/index.php?store_id=1&r=api/",
+  design_method: "3"
+};
